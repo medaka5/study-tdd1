764294a35ead347e1ff0454658117982b90de5fd
Hamano Medaka <hamano.medaka@gmail.com>
2015-03-07 17:36:48 +0900

    use expect, given

--- stdio/test/SprintfTest.c
+++ stdio/test/SprintfTest.c
@@ -7,24 +7,33 @@
 TEST_GROUP(sprintf);
 
 static char output[150];
+static const char *expected;
 TEST_SETUP(sprintf)
 {
 	memset(output, 0xaa, sizeof(output));
+	expected="";
 }
 TEST_TEAR_DOWN(sprintf)
 {
 }
+static void expect(const char *s)
+{
+	expected=s;
+}
+static void given(int charsWitten)
+{
+	size_t expected_size = strlen(expected);
+	TEST_ASSERT_EQUAL(expected_size, charsWitten);
+	TEST_ASSERT_EQUAL_STRING(expected, output);
+	TEST_ASSERT_BYTES_EQUAL(0xaa, output[expected_size + 1]);
+}
 TEST(sprintf, NoFormatOperations)
 {
-	memset(output, 0xaa, sizeof(output));
-	TEST_ASSERT_EQUAL(3, sprintf(output, "hey"));
-	TEST_ASSERT_EQUAL_STRING("hey", output);
-	TEST_ASSERT_BYTES_EQUAL(0xaa, output[4]);
+	expect("hey");
+	given(sprintf(output, "hey"));
 }
 TEST(sprintf, InsertString)
 {
-	memset(output, 0xaa, sizeof(output));
-	TEST_ASSERT_EQUAL(12, sprintf(output, "Hello %s\n", "World"));
-	TEST_ASSERT_EQUAL_STRING("Hello World\n", output);
-	TEST_ASSERT_BYTES_EQUAL(0xaa, output[13]);
+	expect("Hello World\n");
+	given(sprintf(output, "Hello %s\n", "World"));
 }
