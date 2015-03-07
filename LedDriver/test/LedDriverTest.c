cbaa788f8c8175bc0d49c001f61fe89973ef1290
Hamano Medaka <hamano.medaka@gmail.com>
2015-03-07 19:55:35 +0900

    [LedDriver] refactored using setup

--- LedDriver/test/LedDriverTest.c
+++ LedDriver/test/LedDriverTest.c
@@ -12,29 +12,25 @@ static uint16_t virtualleds;
 TEST_SETUP(LedDriver)
 {
     virtualleds = 0xFFFF;
+    LedDriver_Create(&virtualleds);
 }
 TEST_TEAR_DOWN(LedDriver)
 {
 }
 TEST(LedDriver, AllOffAfterCreate)
 {
-    LedDriver_Create(&virtualleds);
     TEST_ASSERT_EQUAL_HEX16(0, virtualleds);
 }
 
 
 TEST(LedDriver, TurnOnLed1)
 {
-    LedDriver_Create(&virtualleds);
-    TEST_ASSERT_EQUAL_HEX16(0, virtualleds);
     LedDriver_TurnOn(1);
     TEST_ASSERT_EQUAL_HEX16(0x0001, virtualleds);
 }
 
 TEST(LedDriver, TurnOffLed1)
 {
-    LedDriver_Create(&virtualleds);
-    TEST_ASSERT_EQUAL_HEX16(0, virtualleds);
     LedDriver_TurnOn(1);
     LedDriver_TurnOff(1);
     TEST_ASSERT_EQUAL_HEX16(0x0000, virtualleds);
