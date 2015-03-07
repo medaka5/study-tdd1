940feff95b4f925631763df19d06fd658992fc7a
Hamano Medaka <hamano.medaka@gmail.com>
2015-03-07 17:48:51 +0900

    [LedDriver] start from here

---
+++ LedDriver/test/LedDriverTest.c
@@ -0,0 +1,18 @@
+#include "unity.h"
+#include "unity_fixture.h"
+#include <stdio.h>
+#include <string.h>
+
+
+TEST_GROUP(LedDriver);
+
+TEST_SETUP(LedDriver)
+{
+}
+TEST_TEAR_DOWN(LedDriver)
+{
+}
+TEST(LedDriver, AllOffAfterCreate)
+{
+	TEST_FAIL_MESSAGE("Start here");
+}
